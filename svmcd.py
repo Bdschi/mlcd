9cd65c31249ec92fba589ecb74d36a5784d7cf40
bdschi <kadmus@nexgo.de>
2024-01-24 23:32:14 +0100

    Correcting typos

--- svmcd.py
+++ svmcd.py
@@ -5,8 +5,7 @@ import numpy as np
 
 # Read the data from a csv file
 D = np.genfromtxt('data.csv', delimiter=',')
-# Remove the rows from D where the last column is empty
-# X is the array without the last column for whichn tge last cokumn is not empty
+# X is the array without the last column for whichn the last column is not empty
 X = D[-np.isnan(D).any(axis=1), :-1]
 # y is the last column of the array when it is not empty
 y = D[-np.isnan(D).any(axis=1), -1]
@@ -22,6 +21,11 @@ X_imputed = imputer.fit_transform(X)
 svc = svm.SVC(kernel='rbf')
 svc.fit(X_imputed, y)                
 y_pred = svc.predict(X_imputed)
+
+# How good is the prediction
+print(np.mean(y_pred == y))
+# what is the accuracy of the prediction
+print(svc.score(X_imputed, y))
 
 # Print the predictions
 print(y_pred)
