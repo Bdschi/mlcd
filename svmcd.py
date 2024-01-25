df66187af39c02ffe4cc1c130b2fa4e587ffaa40
bdschi <bdschi@NUEL-9V8XFB3.gfk.com>
2024-01-25 10:26:07 +0100

    fix array issues.

--- svmcd.py
+++ svmcd.py
@@ -3,15 +3,22 @@ from sklearn import svm
 from sklearn.impute import KNNImputer
 import numpy as np
 
+rows1=100
+rows2=100
+columns=10
+
 # Read the data from a csv file
 D = np.genfromtxt('data.csv', delimiter=',')
 # X is the array without the last column for whichn the last column is not empty
-X = D[-np.isnan(D).any(axis=1), :-1]
+X=D[:rows1-1,:columns-1]
+print("X=",X)
 # y is the last column of the array when it is not empty
-y = D[-np.isnan(D).any(axis=1), -1]
+y=D[:rows1-1,columns-1]
+print("y",y)
 
 # X_new is the array with the missing labels
-X_new = D[np.isnan(D).any(axis=1), :-1]
+X_new=D[rows1-1:rows1+rows2-1,:columns-1]
+print("X_new=",X_new)
                                     
 # Use the KNNImputer to impute the missing values
 imputer = KNNImputer(n_neighbors=2)
