90e7c035c50dcdee237eb64ae159d61b5f62c83a
bdschi <bdschi@NUEL-9V8XFB3.gfk.com>
2024-01-25 13:21:55 +0100

    Using config file to control size of data.

--- svmcd.py
+++ svmcd.py
@@ -2,30 +2,30 @@
 from sklearn import svm
 from sklearn.impute import KNNImputer
 import numpy as np
-
-rows1=100
-rows2=100
-columns=10
+import mlcdconfig
 
 # Read the data from a csv file
 D = np.genfromtxt('data.csv', delimiter=',')
 # X is the array without the last column for whichn the last column is not empty
-X=D[:rows1-1,:columns-1]
+X=D[:mlcdconfig.rows1-1,:mlcdconfig.columns-1]
 print("X=",X)
 # y is the last column of the array when it is not empty
-y=D[:rows1-1,columns-1]
+y=D[:mlcdconfig.rows1-1,mlcdconfig.columns-1]
 print("y",y)
 
 # X_new is the array with the missing labels
-X_new=D[rows1-1:rows1+rows2-1,:columns-1]
+X_new=D[mlcdconfig.rows1-1:mlcdconfig.rows1+mlcdconfig.rows2-1,:mlcdconfig.columns-1]
 print("X_new=",X_new)
                                     
 # Use the KNNImputer to impute the missing values
 imputer = KNNImputer(n_neighbors=2)
 X_imputed = imputer.fit_transform(X)
+print("X_imputed=",X_imputed)
 
 # Use the SVC with the RBF kernel to fit and predict the data
-svc = svm.SVC(kernel='rbf')
+svc = svm.SVC(kernel='rbf', C=1, gamma=1)
+
+## Use the SVC with the RBF kernel to fit and predict the data
 svc.fit(X_imputed, y)                
 y_pred = svc.predict(X_imputed)
 
