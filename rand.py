109f0fd47a0dbb85b1eb3f3888b2c439369abf56
bdschi <bdschi@NUEL-9V8XFB3.gfk.com>
2024-01-25 10:25:12 +0100

    Craete date with empty columns

--- rand.py
+++ rand.py
@@ -2,10 +2,28 @@
 
 import random
 
-sum=0
-for i in range(1,10):
-    for j in range(1,10):
-        number = random.randint(0,10)
-        sum+=number
-        print(number,end=",")
+rows1=100
+rows2=100
+columns=10
+for i in range(1,rows1):
+    sum=0
+    for j in range(1,columns):
+        number = random.randint(-10,10)
+        if number>=0:
+            sum+=number
+            print(number,end=",")
+        else:
+            sum+=-number
+            print("",end=",")
     print(sum)
+
+for i in range(1,rows2):
+    for j in range(1,columns):
+        number = random.randint(-10,10)
+        if number>=0:
+            sum+=number
+            print(number,end=",")
+        else:
+            sum+=-number
+            print("",end=",")
+    print()
