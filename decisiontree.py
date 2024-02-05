0b062230f3086a837023a3ea6649539ab2442ec2
bdschi <bdschi@NUEL-9V8XFB3.gfk.com>
2024-02-05 09:04:13 +0100

    I am confused.

--- decisiontree.py
+++ decisiontree.py
@@ -1,24 +1,70 @@
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+import mlcdconfig
+import csv
+import random
 
-# Load the data
-data = np.loadtxt('articles.txt', delimiter='\t', dtype='float32') # Assuming the data is stored in 'articles.txt'
-X = data[:, :-1] # Features are stored in the first 'n-1' columns
-y = data[:, -1] # Category is stored in the last column
+level=2
+categories=[]
+features=[]
+pgs=[]
+file="/mnt/c/Users/bdschi/Downloads/artikelstruktur_(pg_measures)_559510.csv"
+with open(file, "r") as csvfile:
+    lines = csv.reader(csvfile, delimiter=';', quotechar='"')
+    for line in lines:
+        text = line[0]
+        if text[0] == '"':
+            text=text[1:]
+        if text[-1] == '"':
+            text=text[:-1]
+        atexts=text.split("\\")
+        if len(atexts) < 6:
+            continue
+        if atexts[level][0] == " ":
+            atexts[level]=atexts[level][1:]
+        if atexts[5][0] == " ":
+            atexts[5]=atexts[5][1:]
+        pgs.append(atexts[5])
+        measures=[]
+        for im in range(2, 5):
+            measures.append(float(line[im].replace(",", ".")))
+        #print("category=\"%s\" pg=\"%s\" measures=%s" % (atexts[level], atexts[5], measures))
+        categories.append(atexts[level])
+        features.append(measures)
+    print("number of different product groups %d" % len(set(pgs)))
+    pgset=set(pgs)
+    #for i in range(len(pgset)):
+    #    print("i=%d pg=\"%s\"" % (i, list(pgset)[i]))
+    newfeatures=[]
+    emptypgs=len(set(pgs))*[0.0]
+    for i in range(len(categories)):
+        newfeature=features[i]+emptypgs
+        #print("pg=\"%s\" index=%d" % (pgs[i], list(pgset).index(pgs[i])))
+        newfeature[3+list(pgset).index(pgs[i])]=1.0
+        newfeatures.append(newfeature)
+        #print("newfeature=%s" % newfeature)
+    randomindices=[i for i in range(0, len(categories))]
+    random.shuffle(randomindices)
+    train_features = [newfeatures[i] for i in randomindices[:mlcdconfig.rows1]]
+    train_categories = [categories[i] for i in randomindices[:mlcdconfig.rows1]]
+    test_features = [newfeatures[i] for i in randomindices[mlcdconfig.rows1:mlcdconfig.rows1+mlcdconfig.rows2]]
+    test_categories = [categories[i] for i in randomindices[mlcdconfig.rows1:mlcdconfig.rows1+mlcdconfig.rows2]]
 
-# Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    # Create the decision tree classifier
+    classifier = DecisionTreeClassifier()
+    classifier.fit(train_features, train_categories)
 
-# Create the decision tree classifier
-classifier = DecisionTreeClassifier()
-classifier.fit(X_train, y_train)
+    # Make predictions on the test data
+    predictions = classifier.predict(test_features)
 
-# Load an article to test
-article_features = [10, 20, 30] # Replace with the actual features of the article
+    totcnt=0
+    errcnt=0
+    # Print out the actual and predicted categories
+    for i in range(len(test_categories)):
+        totcnt+=1
+        if(test_categories[i] != predictions[i]):
+            errcnt+=1
+            print("actual=\"%s\" predicted=\"%s\"" % (test_categories[i], predictions[i]))
 
-# Predict the category of the article
-predicted_category = classifier.predict([article_features])[0]
-
-print('The predicted category of the article is:', predicted_category)
-
+    print("total=%d errors=%d accuray=%.2f" % (totcnt, errcnt, (totcnt-errcnt)/totcnt))
